b6315e0bfe051a2df78c94a3f649ee16feeca857
Greg-alm <a.gregory@escola.pr.gov.br>
2026-04-08 11:54:39 -0300

    Add files via upload

---
+++ cola_de_codigos.js
@@ -0,0 +1,46 @@
+//1-exiba a mensagem "Ola mundo" no console.
+console.log("Ola mundo");
+//2-crie uma variavel com seu nome e exiba o valor no console.
+let nome= "gregory";
+console.log(nome);
+//3-some dois numeros e exiba o resultado
+
+let numero1= 1;
+let numero2 = 2;
+
+let somar = numero1 + numero2;
+console.log(somar);
+
+
+//forma do professor
+
+let soma = 10+5;
+console.log(soma);
+
+//4-subtraia dois numeros e exiba o valor
+let soma2 = 10-5;
+console.log(soma2);
+
+//5-descubra o resto da divisão de um numero por outro
+let resto = 10 % 3;
+console.log(resto);
+
+//6-crie uma variavel booleana(true false) e exiba seu valor
+
+let estaChovendo = true;
+console.log(estaChovendo);
+
+//7- verifique se um numero é maior que outro e exiba o resultado
+
+n1= 2;
+n2=7;
+if(n1<n2){
+    console.log("menor")
+};
+
+//jeito do professor
+let maiorQue = 18 > 21;
+console.log(maiorQue);
+
+//8-
+
