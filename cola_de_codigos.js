4d3d883413aa90f4b2f4b49b6104533853a2e107
Greg-alm <a.gregory@escola.pr.gov.br>
2026-04-09 09:08:40 -0300

    Add files via upload

--- cola_de_codigos.js
+++ cola_de_codigos.js
@@ -42,5 +42,61 @@ if(n1<n2){
 let maiorQue = 18 > 21;
 console.log(maiorQue);
 
-//8-
+//8-Crie 2 variaveis e exiba a concatenação
 
+let vari1 = "teste1";
+
+let vari2  = "teste2";
+
+console.log(`mostrar ${vari1} ${vari2}`);
+
+//9-descubra o tipo de uma variavel
+let valor = 100;
+console.log(typeof valor); //typeofvmostra o tipo da variavel
+
+//10-converta numero para string
+
+let num = 50;
+console.log(String(num));
+
+//11- string para numero
+
+let texto = "teste";
+console.log(Number(texto));
+
+
+
+//                             LAÇOS DE REPETIÇÃO
+
+//12-exiba no console numero de 0 a 10
+
+for(let i =0; i<=10; i++){
+    console.log(i);
+}
+
+//13-exiba apenas numeros pares até 20
+for(let i =0; i<=20; i+=2){
+    console.log(i);
+}
+//14-mostre no console numeros de 10 ate 1
+for(let i =10; i>=1; i--){
+    console.log(i);
+}
+
+//15- some numero de 1 a 5
+n1 = 1,n2 = 2,n3 = 3,n4 = 4,n5 = 5
+let a = n1+n2+n3+n4+n5;
+console.log(a);
+
+//jeito do professor
+let somaLoop = 0;
+for(let i=1; i<=5; i++){
+    somaLoop+=i;
+    console.log("valor de i"+i+"valor de loop"+somaLoop);
+}
+console.log(somaLoop);
+
+//16- faça a tabuada do 3
+for(let i =0; i<=30; i+=3){
+    console.log(i);
+}
